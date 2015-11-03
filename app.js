ea5e5cdb88b00018d0da532186378aaf6a493479
XuPoH <ilyha19@live.ru>
2015-11-03 12:11:30 +0500

    Minor changes

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var SteamTradeOffers = require('steam-tradeoffers');
 
 var ee = new events.EventEmitter();
 
-ee.on('createServer', function() {
+ee.on('createServer', function () {
 	http.createServer(function (request, response) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
 		response.end('Hello World\n');
 	}).listen(8124);
 });
 
 cmd.pref();
-cmd.add('start_server', function(){
+cmd.add('start_server', function () {
 	ee.emit('createServer');
 });
