dbef1f180e18619ba673a2ca10c7dc54be686f6f
Vladlen <ultrashock98@gmail.com>
2015-11-03 18:02:19 +1000

    LOL
    LOl

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+var steam = require('Steam');
 /* Конфиг */
 var config = require('./config');
-/* NameSpace модуля trade */
-var SteamTradeNameSpace = require('steam-tradeoffers');
-/* Новый объект трейда */
-var trade = new SteamTradeNameSpace();
+/* Steam */
+var steamClient = new steam.SteamClient();
+var steamUser = new steam.SteamUser(steamClient);
 
-/* */
-trade.setup({
-	sessionID: '',
-	webCookie: '',
-	APIKey: 
+//var steamFriends = new steam.SteamFriends(steamClient);
+//var steamWebLogOn = new SteamWebLogOn(steamClient, steamUser);
+
+//var Steam = require('steam');
+//var SteamWebLogOn = require('steam-weblogon');
+//var getSteamAPIKey = require('steam-web-api-key');
+//var SteamTradeOffers = require('steam-tradeoffers')
+
+steamClient.connect();
+steamClient.on('connected',function(){
+	steamUser.logOn({
+		account_name: config.bots[0].Username,
+		password: config.bots[0].Password
+	});
 });
+steamClient.on('disconnected',function(){
+	console.log('Ушол :C');
+});
+steamClient.on('logOnResponse',function(){
+	
+});
+console.log(steam.SteamUser);
