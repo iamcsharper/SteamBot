82ee4081ba290843d8c1a5f945290fb68523bcda
Vladlen <ultrashock98@gmail.com>
2015-11-03 18:42:20 +1000

    TET
    TET

--- app.js
+++ app.js
@@ -1,29 +1,171 @@
-var steam = require('Steam');
-/* Конфиг */
-var config = require('./config');
-/* Steam */
-var steamClient = new steam.SteamClient();
-var steamUser = new steam.SteamUser(steamClient);
-
+//var steam = require('Steam');
+///* Конфиг аккаунтов */
+//var bots = require('./cfg/bots');
+///* Файловая система */
+//var fs = require('fs');
+///* Подгружаем сервера авторизации если есть */
+//if (fs.existsSync('servers')) {
+//	steam.servers = JSON.parse(fs.readFileSync('servers'));
+//}
+///* Steam */
+//var steamClient = new steam.SteamClient();
+//var steamUser = new steam.SteamUser(steamClient);
 //var steamFriends = new steam.SteamFriends(steamClient);
-//var steamWebLogOn = new SteamWebLogOn(steamClient, steamUser);
+//var steamTrade = new steam.SteamTrading(steamClient);
+//
+///* Подключаемся к steam */
+//steamClient.connect();
+///* События для при подключение к steam*/
+//steamClient.on('connected',function(){
+//	/* Подключаем по логину и паролю */
+//	steamUser.logOn({
+//		account_name: bots.bots[0].Username,
+//		password: bots.bots[0].Password
+//	});
+//	steamTrade.trade('76561198080998288');
+//	
+//});
+//
+//steamTrade.on('tradeProposed',function(id,who_id){
+//	console.log(id,who_id);
+//});
+//steamTrade.on('tradeResult',function(){
+//	
+//});
+//steamTrade.on('sessionStart',function(){
+//	
+//});
+//
+//
+///* Когда данные пришли */
+//steamClient.on('logOnResponse',function(res){
+//	console.log(res);
+//});
+///* При диксконекте */
+//steamClient.on('disconnected',function(){
+//	console.log('Ушол :C');
+//});
+///* Обработка ошибок */
+//steamClient.on('error',function(err){
+//	console.log(err);
+//});
+///* Какие то сервера, про малофью несёт */
+//steamClient.on('servers',function(servers){
+//	fs.writeFile('servers',JSON.stringify(servers));
+//});
+var fs = require('fs');
+var crypto = require('crypto');
 
-//var Steam = require('steam');
-//var SteamWebLogOn = require('steam-weblogon');
-//var getSteamAPIKey = require('steam-web-api-key');
-//var SteamTradeOffers = require('steam-tradeoffers')
+var Steam = require('steam');
+var SteamWebLogOn = require('steam-weblogon');
+var getSteamAPIKey = require('steam-web-api-key');
+var SteamTradeOffers = require('steam-tradeoffers'); // change to 'steam-tradeoffers' if not running from the examples subdirectory
+
+var admin = '76561198080998288'; // put your steamid here so the bot can send you trade offers
+
+var logOnOptions = {
+	account_name: 'gleb22897',
+	password: 'valerok228'
+};
+
+var authCode = ''; // code received by email
+
+try {
+	logOnOptions.sha_sentryfile = getSHA1(fs.readFileSync('sentry'));
+} catch (e) {
+	if (authCode !== '') {
+		logOnOptions.auth_code = authCode;
+	}
+}
+
+// if we've saved a server list, use it
+if (fs.existsSync('servers')) {
+	Steam.servers = JSON.parse(fs.readFileSync('servers'));
+}
+
+var steamClient = new Steam.SteamClient();
+var steamUser = new Steam.SteamUser(steamClient);
+var steamFriends = new Steam.SteamFriends(steamClient);
+var steamWebLogOn = new SteamWebLogOn(steamClient, steamUser);
+var offers = new SteamTradeOffers();
 
 steamClient.connect();
-steamClient.on('connected',function(){
-	steamUser.logOn({
-		account_name: config.bots[0].Username,
-		password: config.bots[0].Password
+steamClient.on('connected', function () {
+	steamUser.logOn(logOnOptions);
+});
+
+function offerItems() {
+	offers.loadMyInventory({
+		appId: 440,
+		contextId: 2
+	}, function (err, items) {
+		var item;
+		// picking first tradable item
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].tradable) {
+				item = items[i];
+				break;
+			}
+		}
+		// if there is such an item, making an offer with it
+		if (item) {
+			offers.makeOffer({
+				partnerSteamId: admin,
+				itemsFromMe: [
+					{
+						appid: 440,
+						contextid: 2,
+						amount: 1,
+						assetid: item.id
+          }
+        ],
+				itemsFromThem: [],
+				message: 'This is test'
+			}, function (err, response) {
+				if (err) {
+					throw err;
+				}
+				console.log(response);
+			});
+		}
+	});
+}
+
+steamClient.on('logOnResponse', function (logonResp) {
+	if (logonResp.eresult === Steam.EResult.OK) {
+		console.log('Logged in!');
+		steamFriends.setPersonaState(Steam.EPersonaState.Online);
+
+		steamWebLogOn.webLogOn(function (sessionID, newCookie) {
+			getSteamAPIKey({
+				sessionID: sessionID,
+				webCookie: newCookie
+			}, function (err, APIKey) {
+				offers.setup({
+					sessionID: sessionID,
+					webCookie: newCookie,
+					APIKey: APIKey
+				}, function () {
+					offerItems();
+				});
+			});
+		});
+	}
+});
+
+steamClient.on('servers', function (servers) {
+	fs.writeFile('servers', JSON.stringify(servers));
+});
+
+steamUser.on('updateMachineAuth', function (sentry, callback) {
+	fs.writeFileSync('sentry', sentry.bytes);
+	callback({
+		sha_file: getSHA1(sentry.bytes)
 	});
 });
-steamClient.on('disconnected',function(){
-	console.log('Ушол :C');
-});
-steamClient.on('logOnResponse',function(){
-	
-});
-console.log(steam.SteamUser);
+
+function getSHA1(bytes) {
+	var shasum = crypto.createHash('sha1');
+	shasum.end(bytes);
+	return shasum.read();
+}
