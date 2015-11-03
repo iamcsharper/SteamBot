2861c0b29d95a40998d4c5b57d6cc016d839834c
Vladlen <ultrashock98@gmail.com>
2015-11-03 17:26:53 +1000

    Kek
    Kok

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var http = require('http');
-var events = require('events');
-var cmd = require('cmd');
-var SteamTradeOffers = require('steam-tradeoffers');
+/* NameSpace модуля trade */
+var SteamTradeNameSpace = require('steam-tradeoffers');
+/* Новый объект трейда */
+var trade = new SteamTradeNameSpace();
 
-var ee = new events.EventEmitter();
-
-ee.on('createServer', function () {
-	http.createServer(function (request, response) {
-		response.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
-		response.end('Hello World\n');
-	}).listen(8124);
-});
-
-cmd.pref();
-cmd.add('start_server', function () {
-	ee.emit('createServer');
+/* */
+trade.setup({
+	sessionID: '',
+	webCookie: '',
+	APIKey: 
 });
