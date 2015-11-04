dcdf062354a78cd429b2993ad137b054d3209b66
XuPoH <ilyha19@live.ru>
2015-11-04 20:24:29 +0500

    Общение добавил

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 var async = require('async');
-
 var colors = require('colors');
-
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
-
 var mysql = require('mysql');
-
 var crypto = require('crypto');
-
 var Steam = require('steam');
 var SteamWebLogOn = require('steam-weblogon');
 var getSteamAPIKey = require('steam-web-api-key');
 var SteamTradeOffers = require('steam-tradeoffers'); // change to 'steam-tradeoffers' if not running from the examples subdirectory
+var util = require('util');
 
 var config = require('./cfg/config.json');
 
@@ -65,7 +61,7 @@ function getSHA1(bytes) {
 	return shasum.read();
 }
 
-var friendsInfo = [];
+var friendsInfo = {};
 
 var botUse = [];
 
@@ -170,7 +166,7 @@ async.forEach(config.Bots, function (bot, botCallback) {
 						webCookie: newCookie,
 						APIKey: APIKey
 					}, function () {
-						console.log(logPref + 'Высылаем все вещи нашему дорогому администратору!');
+						console.log(logPref + 'Высылаем все вещи админу!');
 
 						offerItems(config.AdminID);
 					});
@@ -178,6 +174,34 @@ async.forEach(config.Bots, function (bot, botCallback) {
 				});
 
 				console.log(logPref + 'Авторизовались!');
+
+				var keys = [];
+				var result = {};
+
+				/**
+				None: 0,
+				Blocked: 1,
+				RequestRecipient: 2,
+				Friend: 3,
+				RequestInitiator: 4,
+				Ignored: 5,
+				IgnoredFriend: 6,
+				SuggestedFriend: 7,
+				Max: 8
+				**/
+
+				var goodRelations = [Steam.EFriendRelationship.Friend];
+
+				async.forEach(Object.keys(steamFriends.friends), function (sid, nextFriend) {
+					if (!keys[sid] && goodRelations.indexOf(steamFriends.friends[sid]) != -1) {
+						result[sid] = steamFriends.friends[sid];
+						keys.push(sid);
+					}
+
+					nextFriend();
+				}, function (err) {
+					console.log(keys);
+				});
 
 				botUse.push({
 					client: steamClient,
@@ -203,6 +227,9 @@ async.forEach(config.Bots, function (bot, botCallback) {
 		console.log(error);
 	});
 
+	var lastPhrase = -1;
+	var lastCount = 1;
+
 	steamFriends.on('message', function (senderID, message, type) {
 		/** Types are:
 		 * Invalid: 0,
@@ -226,16 +253,75 @@ async.forEach(config.Bots, function (bot, botCallback) {
 			default: console.log(type);
 			break;
 		case 1:
-				console.log(logPref + 'Пользователь ' + senderID + ' написал мне сообщение: "' + message + '"');
 
-			var msgs = ['Здравствуй, ' + senderID, 'Здарова', 'КЕК)0)0'];
+				var phrases = [
+				{
+					pattern: /ч(?:ё|е) там с (?:друзьями|другами)/i,
+					response: function () {
+						return util.inspect(friendsInfo);
+					}
+				},
+				{
+					pattern: /^привет(?:,| |, )(.*)/i,
+					response: function (input, lastCount, who) {
+						who = who.replace(/( )*/, '');
 
-			var pattern = /^привет$/i;
-			var match = message.match(pattern);
+						var msgs = ['И тебе, ' + senderID, 'Угу', 'Ну что ещё?'];
+						var good = ['няша', 'ня', 'бро', 'братан', 'брат', 'бот']
+						var swear = ['сука', 'пидор', 'хуесос', 'кек', 'принцесса', 'блять', 'блядь', 'ска', 'пёс', 'козёл', 'чмо', 'деб', 'дебил', 'дибил'];
 
-			if (match) {
-				steamFriends.sendMessage(senderID, msgs[randomInt(0, msgs.length)]);
-			}
+						if (swear.indexOf(who) > -1) {
+							msgs = ['Будь добр не обижать меня. Я робот ранимый.', 'Усмири свой пыл, сын мой! А то я и обидеться могу...', 'Слышь БРАТКА, НА ЗОНЕ ТАК ОБЩАТЬСЯ БУДЕШЬ!'];
+						} else if (good.indexOf(who) > -1) {
+							msgs = [':3 доброго времени суток! C:', 'И тебе, йоу! ^_^', 'Ихих;3 Приветик'];
+						}
+
+						return msgs[randomInt(0, msgs.length)] + '(' + lastCount + ')';
+					}
+				},
+				{
+					pattern: /привет$/i,
+					response: function (input, lastCount, who) {
+						var msgs = ['И тебе, ' + senderID, 'Угу', 'Ну что ещё?', 'Угу', 'Ага', 'Ну'];
+
+						return msgs[randomInt(0, msgs.length)];
+					}
+				}
+			];
+
+			async.each(phrases, function (phrase, nextPhrase) {
+				var match = message.match(phrase.pattern);
+
+				if (match) {
+					var params = phrase.pattern.exec(message);
+
+					// Если последняя итерация годная и текущая такая же
+					if (lastPhrase == phrases.indexOf(phrase)) {
+						++lastCount;
+					} else {
+						lastCount = parseInt('0');
+					}
+
+					lastPhrase = phrases.indexOf(phrase);
+					params.unshift(params['input'], lastCount);
+
+					delete params['input'];
+					delete params['index'];
+
+					var res = phrase.response.apply(null, params);
+
+					steamFriends.sendMessage(senderID, res);
+
+					console.log(logPref + 'Пользователь ' + senderID + ' написал: "' + message + '"');
+
+					nextPhrase({
+						end: true
+					});
+				} else {
+					nextPhrase();
+				}
+			}, function (end) {});
+
 			break;
 		case 2:
 				console.log(logPref + 'Пользователь ' + senderID + ' пишет мне сообщение...');
@@ -251,25 +337,33 @@ async.forEach(config.Bots, function (bot, botCallback) {
 	});
 
 	steamFriends.on('personaState', function (friend) {
-		friendsInfo.push(friend);
+		var avatar_buf = friend.avatar_hash;
+		var avatar_hash = avatar_buf.toString('hex');
+
+		if (avatar_buf.toString().length == 0) {
+			avatar_hash = 'fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb'; // empty steam image, FIXED 15:42 MSC 04.11.2015
+		}
+
+		var avatar_url = util.format('http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/80/%s_full.jpg', avatar_hash);
+
+		var current = {
+			steam_id: friend.friendid,
+			name: friend.player_name,
+			avatar: avatar_url
+		};
+
+		friendsInfo[friend.friendid] = current;
 	});
 }, function (err) {
-	console.log('Загрузка всех ботов завершена!');
-	if (friendsInfo[0]) {
-		var buf = friendsInfo[0].avatar_hash;
-		fs.writeFile(__dirname + '/test.txt', buf.toString('hex'), function (err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
+	console.log('****** Загрузка всех ботов завершена! ******'.green.bold);
 
 	async.forEach(botUse, function (data, closure) {
 		var client = data.client;
-		var botName = data.botName;
+		var botName = data.botName.toString();
 		var sid = client.steamID.toString();
+		var friends = data.friends.friends;
 
-		console.log(botName + ' has id ' + sid);
+		console.log(botName.green.bold + ' has id ' + sid);
 
 		/** Пример вывода:
 		'steamid': statusID
@@ -285,7 +379,5 @@ async.forEach(config.Bots, function (bot, botCallback) {
 		  SuggestedFriend: 7,
 		  Max: 8
 	  	**/
-
-		//console.log(data.friends.friends);
 	});
 });
